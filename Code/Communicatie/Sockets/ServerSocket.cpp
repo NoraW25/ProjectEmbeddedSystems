3664d1559a49cf5ff0e84874a48501d8a1b386e0
Isabel <isabeldelft015@gmail.com>
2026-05-06 12:51:27 +0200

    Debug code

--- Code/Communicatie/Sockets/ServerSocket.cpp
+++ Code/Communicatie/Sockets/ServerSocket.cpp
@@ -94,7 +94,7 @@ bool ServerSocket::heeftOntvangen(){
     } 
     
     ssize_t bytes = read(actieve_socket, buffer, sizeof(buffer) - 1);
-    std::cout<<"heeftOntvangen na read()"<<std::endl;
+    //std::cout<<"heeftOntvangen na read()"<<std::endl;
 
     if (bytes < 0) {
         if (errno == EAGAIN || errno == EWOULDBLOCK) {
