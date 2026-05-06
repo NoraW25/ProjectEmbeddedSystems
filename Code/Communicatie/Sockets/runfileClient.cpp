7256606dcd121a2ac236bbfd76c155b5d667a33a
Isabel <isabeldelft015@gmail.com>
2026-05-06 11:37:15 +0200

    Test met verzenden client

--- Code/Communicatie/Sockets/runfileClient.cpp
+++ Code/Communicatie/Sockets/runfileClient.cpp
@@ -7,6 +7,8 @@ int main(int argc, char const* argv[])
 {
     ClientSocket* socket = ClientSocket::instantie();
 
+    socket->versturen("Hello there!");
+
     while (true){
         if(socket->heeftOntvangen()){
             std::cout<<socket->ontvangst()<<std::endl;
