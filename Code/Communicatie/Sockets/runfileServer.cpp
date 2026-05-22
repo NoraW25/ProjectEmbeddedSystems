c1f4a64eb29fe6859d14470d8e18ad89e9952f0b
Isabel <isabeldelft015@gmail.com>
2026-05-22 15:47:26 +0200

    Debug CanSocket

--- Code/Communicatie/Sockets/runfileServer.cpp
+++ Code/Communicatie/Sockets/runfileServer.cpp
@@ -39,8 +39,8 @@ int main(int argc, char const* argv[])
             std::cout<<str<<std::endl;
             std::cout<<"Ontvangen"<<std::endl;
             int value = 0;
-            scanf_s(str.c_str(), "%d", value);
-            std::string text = "ID:410;DCL:1;Data:" + value + ";";
+            scanf(str.c_str(), "%d", value);
+            std::string text = "ID:410;DCL:1;Data:" + value.to_string() + ";";
             socketCan->sendSocket(text);
         }
     }
