625d53ca39243fa655582c69fb7958aa7d8078e4
Isabel <isabeldelft015@gmail.com>
2026-06-01 10:57:33 +0200

    Debug CanReceiver

--- Code/Communicatie/CanCom/CanCom/CanReceiver.cpp
+++ Code/Communicatie/CanCom/CanCom/CanReceiver.cpp
@@ -27,14 +27,14 @@ bool CanReceiver::receive(int* address, std::vector<uint8_t>* data){
     return true;
 }
 
-int CanSocket::parseId(const std::string& message){
+int CanReceiver::parseId(const std::string& message){
     size_t key = message.find(key_id) + key_id.length();
     size_t value = message.find(";", key);
     int id = std::stoi(message.substr(key, value - key));
     return id;
 }
 
-std::vector<uint8_t> CanSocket::parseData(const std::string& message){
+std::vector<uint8_t> CanReceiver::parseData(const std::string& message){
     std::vector<uint8_t> result;
 
     size_t position = message.find(key_data) + key_data.length();
