c4b49807a28f898b2ed81436d9442105dcdfa179
Isabel <isabeldelft015@gmail.com>
2026-06-05 10:35:19 +0200

    Debug messagetranslator

--- Code/Communicatie/RPiB/CanCom/Translator/MessageTranslator.cpp
+++ Code/Communicatie/RPiB/CanCom/Translator/MessageTranslator.cpp
@@ -33,6 +33,7 @@ void MessageTranslator::translate(int* id, std::vector<uint8_t>* data, std::stri
 
 std::string MessageTranslator::translate(int id, std::vector<uint8_t> data){
     std::string text = key_id + stringifyId(id) + ";"+ key_data + stringifyData(data);
+    std::cout<<"Size in translate: "<<std::to_string(data.size())<<std::endl;
     std::cout<<"Text in translate MessageTranslator: id + data naar string:\n"<<text<<"\n"<<std::endl;
     return text;
 }
