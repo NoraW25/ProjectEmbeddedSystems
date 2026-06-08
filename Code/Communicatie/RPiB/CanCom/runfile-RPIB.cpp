a09040077676441538d1af827229bc3a9364c8ea
Arne_Jansonius <arnejansonius@outlook.com>
2026-06-08 16:39:36 +0200

    test

--- Code/Communicatie/RPiB/CanCom/runfile-RPIB.cpp
+++ Code/Communicatie/RPiB/CanCom/runfile-RPIB.cpp
@@ -80,13 +80,13 @@ int main()
 
     // Aanmaken van een klasse die een proces aanstuurd
     ClimateSystem climate_system(client_controller_wemos_klimaat);
-
-    runService->createTask(*transmitFunc, 640);
     std::vector<uint8_t> d1 = {20};
     client_controller_wemos_display->transmitData(710, d1);
 
     std::vector<uint8_t> d2 = {5};
     client_controller_wemos_display->transmitData(720, d2);
+    runService->createTask(*transmitFunc, 640);
+
     while (1)
     {
         // printf("updating\n");
