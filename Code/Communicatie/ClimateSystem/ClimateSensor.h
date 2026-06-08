0a8d34af77baf5ec0f2533335233a5f097955376
Isabel <isabeldelft015@gmail.com>
2026-06-08 08:54:05 +0200

    Debug includes

--- Code/Communicatie/ClimateSystem/ClimateSensor.h
+++ Code/Communicatie/ClimateSystem/ClimateSensor.h
@@ -4,8 +4,10 @@
 
 #include <memory>
 #include <vector>
-#include "..\RPiB\CanCom\CommunicationController.h"
+//#include "..\RPiB\CanCom\CommunicationController.h"
 #include "ClimateSystem.h"
+
+class Communication::CommunicationController;
 
 class ClimateSystem;
 
