c635835fe8a8524ef7e87b1d25544154307327ac
Isabel <isabeldelft015@gmail.com>
2026-06-11 13:47:52 +0200

    Verlichting aan/uit toegevoegd RPiA

--- Code/Communicatie/CanCom/CanCom/LightingSystem.cpp
+++ Code/Communicatie/CanCom/CanCom/LightingSystem.cpp
@@ -11,17 +11,22 @@
 #define COLOURWHITE "wit"
 
 
-LightingSystem::LightingSystem(int button_address, std::shared_ptr<Communication::CommunicationController> controller):
+LightingSystem::LightingSystem(int button_change_colour_address, int button_off_address, std::shared_ptr<Communication::CommunicationController> controller):
     controller(controller),
     button_address(button_address),
     lights_off_address(509),
     lights_on(true){
 
     // Koppel controller aan verander kleur, wordt geactiveerd wanneer de knop wordt ingedrukt.
-    controller->logReceived(button_address, 
+    controller->logReceived(button_change_colour_address, 
         [this](std::vector<uint8_t> data) { changeColour(data); } 
         // Om this af te vangen en alleen het type void (*)(std::vector<uint8_t>) mee te geven ipv void (ClimateSensor::*)(std::vector<uint8_t>)
     ); 
+
+    controller->logReceived(button_off_address, 
+        [this](std::vector<uint8_t> data) { toggleLight(data); } 
+        // Om this af te vangen en alleen het type void (*)(std::vector<uint8_t>) mee te geven ipv void (ClimateSensor::*)(std::vector<uint8_t>)
+    );
 
     colours[COLOURORANGE] = 501;
     colours[COLOURRED] = 502;
