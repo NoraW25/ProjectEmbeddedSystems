c3262238981022564c0205aedc0f38ee4784b696
Isabel <isabeldelft015@gmail.com>
2026-06-11 16:19:59 +0200

    Debug AlarmSystem

--- Code/Communicatie/CanCom/CanCom/AlarmSystem.cpp
+++ Code/Communicatie/CanCom/CanCom/AlarmSystem.cpp
@@ -1,6 +1,8 @@
 
 #include "AlarmSystem.h"
 
+#include <iostream>
+
 AlarmSystem::AlarmSystem(std::shared_ptr<Communication::CommunicationController> controller_can, 
     std::shared_ptr<Communication::CommunicationController> controller_rpib, 
     int address):
@@ -16,5 +18,6 @@ AlarmSystem::AlarmSystem(std::shared_ptr<Communication::CommunicationController>
 }
 
 void AlarmSystem::soundAlarm(std::vector<uint8_t> data){
+    std::cout<<"In alarm systeem"<<std::endl;
     controller_can->transmitData(alarm_address, data);
 }
