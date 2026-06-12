a14442e8190cd166b2dd4693fcb481fa1c8ee704
Isabel <isabeldelft015@gmail.com>
2026-06-12 14:34:25 +0200

    Debug runfile RPiB

--- Code/Communicatie/RPiB/CanCom/runfile-RPIB.cpp
+++ Code/Communicatie/RPiB/CanCom/runfile-RPIB.cpp
@@ -35,7 +35,7 @@ int main()
     ClientGeneraliser generaliser_client_rpia("192.168.0.2", false);
     ClientGeneraliser generaliser_client_wemos_klimaat("192.168.0.101", true);
     ClientGeneraliser generaliser_client_wemos_display("192.168.0.103", true);
-    //ClientGeneraliser generaliser_client_wemos_hartslag("192.168.0.102", true);
+    ClientGeneraliser generaliser_client_wemos_hartslag("192.168.0.102", true);
     ClientGeneraliser generaliser_client_wemos_encoder("192.168.0.104", true);
 
     // De controllers aanmaken en binden aan de controller
@@ -54,10 +54,10 @@ int main()
             &generaliser_client_wemos_display,
             &generaliser_client_wemos_display);
 
-    // std::shared_ptr<Communication::CommunicationController> client_controller_wemos_hartslag =
-    //     std::make_shared<Communication::CommunicationController>(
-    //         &generaliser_client_wemos_hartslag,
-    //         &generaliser_client_wemos_hartslag);
+    std::shared_ptr<Communication::CommunicationController> client_controller_wemos_hartslag =
+        std::make_shared<Communication::CommunicationController>(
+            &generaliser_client_wemos_hartslag,
+            &generaliser_client_wemos_hartslag);
 
     std::shared_ptr<Communication::CommunicationController> client_controller_wemos_encoder =
         std::make_shared<Communication::CommunicationController>(
@@ -67,8 +67,8 @@ int main()
     // Aanmaken van een klasses die een processen aansturen
     ClimateSystem climate_system(client_controller_wemos_klimaat, client_controller_rpia, BUZZERADDRESS);
     DistanceSystemRPiB distance_system(client_controller_rpia, client_controller_wemos_display, client_controller_wemos_encoder, LEDBARADDRESS);
-    // std::shared_ptr<HeartrateDisplayer> heartrateDisplayer = std::make_shared<HeartrateDisplayer>(
-    //     client_controller_wemos_display, client_controller_wemos_hartslag, client_controller_rpia);
+    std::shared_ptr<HeartrateDisplayer> heartrateDisplayer = std::make_shared<HeartrateDisplayer>(
+        client_controller_wemos_display, client_controller_wemos_hartslag, client_controller_rpia);
 
 
     while (1)
