58e2df921ce4efc52f8e481766bacd093c58c3f6
Isabel <isabeldelft015@gmail.com>
2026-06-12 14:33:03 +0200

    Debug amount of lights

--- Code/Communicatie/CanCom/CanCom/DistanceSystem.cpp
+++ Code/Communicatie/CanCom/CanCom/DistanceSystem.cpp
@@ -55,6 +55,7 @@ DistanceSystem::DistanceSystem(std::shared_ptr<Communication::CommunicationContr
 void DistanceSystem::resetDistance(std::vector<uint8_t> data){
     printf("Debug in resetDistance\n");
     distance = 0;
+    amount_of_lights = 0;
     calculateAmountOfLights();
 }
 
