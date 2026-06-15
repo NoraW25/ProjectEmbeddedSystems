f029c29394195d5f8396834b1a26f626fd5cdbf9
Isabel <isabeldelft015@gmail.com>
2026-06-15 22:12:07 +0200

    Documentatie doxygen Clock

--- Code/Communicatie/CanCom/CanCom/Clock.h
+++ Code/Communicatie/CanCom/CanCom/Clock.h
@@ -1,4 +1,12 @@
 
+/*!
+ * @file Clock.h
+ * @class Clock
+ * @brief Stuurt de klok op de CANbus periodiek updates.
+ * 
+ * Updates de klok op de CANbus periodiek met de accurate lokale tijd.
+ */
+
 #ifndef CLOCK_H
 #define CLOCK_H
 
@@ -12,9 +20,31 @@
 
 class Clock {
 public:
+    /*!
+     * @brief Constructor Clock
+     *
+     * De standard en meegegeven waarden worden meegegeven in de attributen.
+     * 
+     * Via de cancontroller wordt de runservice opgehaald en er wordt een 
+     * bericht ingepland over een seconden om de tijd te updaten via 
+     * methode updateClock().
+     * 
+     * @param int - Het adres waar de klok op de CAN naar luistert.
+     * @param shared_ptr<Communication::CommunicationController> - De communicatiecontroller van de CANbus.
+     */
     Clock(int, std::shared_ptr<Communication::CommunicationController>);
+
     virtual ~Clock() = default;
 
+    /*!
+     * @brief Stuurt periodiek een tijd update
+     *
+     * Vraagt de locale tijd op en stuurt deze op de CANbus met het
+     * aangegeven adres.
+     * 
+     * Plant om nog een keer te zenden over een seconden, door
+     * nog een keer deze methode mee te geven in de runservice.
+     */
     void updateClock();
 
 private:
