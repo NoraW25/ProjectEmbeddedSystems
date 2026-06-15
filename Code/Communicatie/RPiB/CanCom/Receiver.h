cb3df7a2949405d20324240fdfe0a6342b5addb7
Isabel <isabeldelft015@gmail.com>
2026-06-15 20:55:26 +0200

    Receiver en Transmitter van de Communicatie Package gedocumenteerd.

--- Code/Communicatie/RPiB/CanCom/Receiver.h
+++ Code/Communicatie/RPiB/CanCom/Receiver.h
@@ -1,3 +1,12 @@
+
+/*!
+ * @file Receiver.h
+ * @class Receiver
+ * @brief Interface klasse voor de Receiver in de Communicatie package
+ * 
+ * Regelt het ontvangen en van berichten op een socket.
+ */
+
 #pragma once
 
 #include <vector>
@@ -8,7 +17,20 @@ namespace Communication {
 	class Receiver {
 	public:
 		Receiver()=default;
+
 		virtual ~Receiver() = default;
+
+		/*!
+		* @brief Checkt of er een bericht is ontvangen.
+		*
+		* Checkt of er een bericht is ontvangen, zo ja wordt de data opgeslagen
+		* in de pointers die meegegeven worden als parameters.
+		* 
+		* @param int* - Een pointer naar een plaats waar het adres van het ontvangen bericht opgeslagen kan worden.
+		* @param vector<uint8_t>* - Een pointer naar een plaats waar de data van het ontvangen bericht opgeslagen kan worden.
+		* 
+		* @return bool - Geeft terug of er een bericht is ontvangen. True, als er een bericht is ontvangen. False, als er GEEN bericht is ontvangen.
+		*/
 		virtual bool receive(int* address, std::vector<uint8_t>* data) = 0;
 	};
 }
--- Code/Communicatie/RPiB/CanCom/Transmitter.h
+++ Code/Communicatie/RPiB/CanCom/Transmitter.h
@@ -1,3 +1,12 @@
+
+/*!
+ * @file Transmitter.h
+ * @class Transmitter
+ * @brief Interface klasse voor de Transmitter in de Communicatie package
+ * 
+ * Regelt het versturen van berichten op een socket voor een controller.
+ */
+
 #pragma once
 
 #include <vector>
@@ -8,7 +17,24 @@ namespace Communication {
 	public:
 		Transmitter() = default;
 		~Transmitter() = default;
+
+		/*!
+		* @brief Verstuurt een bericht.
+		*
+		* Verstuurt een bericht naar het meegegeven adres.
+		* 
+		* @param int - Het adres waar het bericht naartoe verzonden moet worden.
+		* @param vector<uint8_t>* - De data die in het bericht meegegeven moet worden.
+		*/
 		virtual void sendData(int address, std::vector<uint8_t> data) = 0;
+
+		/*!
+		* @brief Verstuurt een bericht zonder data.
+		*
+		* Verstuurt een bericht zonder data naar het meegegeven adres.
+		* 
+		* @param int - Het adres waar het bericht naartoe verzonden moet worden.
+		*/
 		virtual void sendRequest(int address) = 0;
 	};
 }
