8b770f4a76d63021cbdf71fe1e37ca08c4ae411a
Isabel <isabeldelft015@gmail.com>
2026-06-15 22:28:17 +0200

    Documentatie doxygen, klasse LightingSystem

--- Code/Communicatie/CanCom/CanCom/LightingSystem.h
+++ Code/Communicatie/CanCom/CanCom/LightingSystem.h
@@ -1,4 +1,13 @@
 
+/*!
+ * @file LightingSystem.h
+ * @class LightingSystem
+ * @brief Regelt de verlichting.
+ * 
+ * Zorgt ervoor dat de verlichting aan/uit gezet kan worden.
+ * Daarnaast kan de kleur van het licht worden aangepast.
+ */
+
 #ifndef LIGHTING_H
 #define LIGHTING_H
 
@@ -12,14 +21,66 @@
 
 class LightingSystem {
 public:
+    /*!
+     * @brief Constructor LightingSystem
+     *
+     * De standard en meegegeven waarden worden meegegeven in de attributen.
+     * 
+     * De meegegeven controllers worden gekoppeld aan de ontvangst op:
+     * De berichten met adres/ID = standaard op int parameter 1, changeColour(), 
+     *  de cancontroller voor de verander kleur knop.
+     * De berichten met adres/ID = standaard op int parameter 2, toggleLight(), 
+     *  de cancontroller voor de aan/uit knop.
+     * 
+     * Daarnaast worden alle kleuren in een map geplaatst en de iterator op
+     * de eeste plek in de map gezet.
+     * 
+     * @param int - Het adres waar veranderingen voor de kleur van het licht op binnenkomen.
+     * @param int - Het adres waar veranderingen van de status van het licht op binnenkomen.
+     * @param shared_ptr<Communication::CommunicationController> - De communicatiecontroller van de CANbus.
+     */
     LightingSystem(int, int, std::shared_ptr<Communication::CommunicationController>);
 
     virtual ~LightingSystem() = default;
 
+
+    /*!
+     * @brief Verandert de kleur van het licht.
+     *
+     * Checkt eerst of het licht aanstaat, zo ja volgt de rest:
+     *
+     * Schuift de iterator van de map 1 plek op en checkt of die opnieuw
+     * door de map moet loopen om naar de volgende kleur te gaan.
+     * 
+     * Roept daarna sendNewColour() aan om de nieuwe kleur ook echt te 
+     * versturen.
+     * 
+     * @param vector<uint8_t> - De meegegeven data, wordt nu niks meegedaan alleen meegegeven.
+     */ 
     virtual void changeColour(std::vector<uint8_t>);
+
+    /*!
+     * @brief Zet het licht aan/uit.
+     *
+     * Zet de lichten uit wanner ze aan zijn, en zet de lichten aan wanneer ze uit zijn.
+     * Wanneer er de lichten aan gezet worden, wordt de methode sendNewColour() aangeroepen.
+     * Anders verzend deze methode zelf een bericht op de CANbus op het adres waar het licht
+     * naar luistert als het uit gezet moet worden.
+     * 
+     * @param vector<uint8_t> - De meegegeven data, wordt nu niks meegedaan alleen meegegeven.
+     */
     virtual void toggleLight(std::vector<uint8_t>);
+
+    /*!
+     * @brief Verzend een bericht om een kleur aan te zetten.
+     *
+     * Elke kleur luistert naar zijn eigen adres, deze staan vast in de map
+     * De iterator geeft aan welke kleur aan de beurt is, als value heeft
+     * de kleur een adres.
+     * 
+     * Op dat adres wordt in deze methode een bericht verstuurd op de CANbus via de cancontroller.
+     */
     virtual void sendNewColour();
-    // virtual void addLamp();
 
 private:
     std::map<std::string, int>::iterator current_colour;
